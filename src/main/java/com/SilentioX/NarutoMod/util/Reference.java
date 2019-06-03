8fb380746dd6c77f9ea9cbd5972d3b6cca053d33
SilentioX <51011494+SilentioX@users.noreply.github.com>
2019-06-03 11:58:13 -0500

    Create Reference.java

---
+++ src/main/java/com/SilentioX/NarutoMod/util/Reference.java
@@ -0,0 +1,13 @@
+package com.SilentioX.NarutoMod.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "nm";
+	public static final String NAME = "Naruto Mod";
+	public static final String VERSION = "1.0.1";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.SilentioX.NarutoMod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.SilentioX.NarutoMod.proxy.CommonProxy";
+	
+	public static final int ENTITY_HIRAISHIN = 120;
+}
