d87926e448b6c32613f2d44484347b4950e46f08
SilentioX <51011494+SilentioX@users.noreply.github.com>
2019-06-03 11:58:46 -0500

    Create IHasModel.java

---
+++ src/main/java/com/SilentioX/NarutoMod/util/IHasModel.java
@@ -0,0 +1,13 @@
+package com.SilentioX.NarutoMod.util;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public interface IHasModel 
+{
+	public void registerModels();
+}
